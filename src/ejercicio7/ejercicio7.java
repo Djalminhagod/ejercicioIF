87dc3472703f89b1a097390261e666a49a607e66
Djalminhagod <r.senra@liceolapaz.net>
2025-10-02 12:06:55 +0200

    ejercicio7 teminado

---
+++ src/ejercicio7/ejercicio7.java
@@ -0,0 +1,32 @@
+package ejercicio7;
+
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class ejercicio7 {
+    public static void main(String[] args) {
+        System.out.println("******************************************");
+        System.out.println("******************************************");
+        System.out.println("****           Bienvenido             ****");
+        System.out.println("******************************************");
+        System.out.println("******************************************");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Ingrese nota(0 a 100: ");
+        int nota = sc.nextInt();
+        if (nota>=0 && nota<=100){
+            if(nota<=59){
+                System.out.println("F Supenso");
+            }else if(nota<=69){
+                System.out.println("D Suficiente");
+            }else if(nota<=79){
+                System.out.println("C Aprovado");
+            }else if(nota<=89){
+                System.out.println("B Notable");
+            }else if(nota>=90 && nota<=100){
+                System.out.println("A Sobresaliente");
+            }
+        }else {
+            System.out.println("error numero incorrecto");
+        }
+    }
+}
